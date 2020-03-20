ac9a7936d99c687dcc99fc85b0654aed9be1ca86
ykgk518 <y.518gaku@gmai.com>
2020-03-21 03:00:03 +0900

    とりあえず送信可能

---
+++ sample/sample.py
@@ -0,0 +1,12 @@
+from slackweb import Slack
+
+web_hook_url = "webHookのURL"
+
+
+def main():
+    slack = Slack(web_hook_url)
+    slack.notify(text='通知でますか？')
+
+
+if __name__ == "__main__":
+    main()
