17e5b69875351142fc73adbb06a4b74417dbc43d
ykgk518 <y.518gaku@gmail.com>
2020-04-29 07:12:52 +0900

    TEST:timer().run()

---
+++ tests/timer_tests.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+sys.path.append(os.getcwd())
+from src.job import Job
+from src.timer import Timer
+import unittest
+from datetime import datetime, timedelta
+
+
+class TimerTests(unittest.TestCase):
+
+    def test_run(self):
+        """
+        テスト観点:Jobが定時実行されているか
+            1.期待された時間で実行されているか
+            2.Jobが正常に実行されているか
+        """
+        now = datetime.now()
+        expected_time = now + timedelta(seconds=5)
+        execution_time = now + timedelta(seconds=4)
+        job_test = JobTest()
+
+        # 1秒の誤差は許容する前提
+        self.assertTrue(expected_time >= Timer(execution_time, job_test).run())
+
+
+class JobTest(Job):
+    """
+    Jobクラスを継承したテストクラス
+    """
+
+    def run(self):
+        """
+        Returns:
+            datatime: 現在時刻を返却する
+        """
+        return datetime.now()
+
+
+if __name__ == "__main__":
+    unittest.main()
