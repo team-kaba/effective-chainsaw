38d27329067d1490def206400418925ac509495d
ykgk518 <y.518gaku@gmail.com>
2020-05-13 21:05:18 +0900

    implements test for validate method

--- slack-reminder/client/src/reminder/util/ValidateCheck.test.js
+++ slack-reminder/client/src/reminder/util/ValidateCheck.test.js
@@ -1,7 +1,24 @@
 import validateMessageCheck from './ValidateCheck';
 
 
-test('', () => {
+test('Test for valid message', () => {
     const message = '11:00 test\n11:00 test1\n';
     expect(validateMessageCheck(message)).toBeTruthy();
 });
+
+test('Test for invalid number format', () => {
+    const invalidHours = '1:00 test\n';
+    const invalidMinutes = '11:0 test\n';
+    expect(validateMessageCheck(invalidHours)).toBeFalsy();
+    expect(validateMessageCheck(invalidMinutes)).toBeFalsy();
+});
+
+test('Test for non-whitespace format', () => {
+    const nonWhitespace = '11:00test\n';
+    expect(validateMessageCheck(nonWhitespace)).toBeFalsy();
+});
+
+test('Testfor non-Colon format', () => {
+    const nonColon = '11;00 test';
+    expect(validateMessageCheck(nonColon)).toBeFalsy();
+})
